69571038fe93cad6bf117bdfa550ece38da77aa7
Newport2005 <anewportd@hotmail.com>
2024-07-15 14:11:07 +0100

    Primer commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
